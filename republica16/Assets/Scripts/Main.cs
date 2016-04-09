64e652377d49e92975c8ee65b5f21a8fdf76bb24
bickhofe <peter@bickhofe.de>
2016-04-09 09:32:06 +0200

    commit islands project

---
+++ republica16/Assets/Scripts/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+    List<Player> players = new List<Player>();    // a real-world example of declaring a List of 'GameObjects'
+    public Player Player0;
+    public Player Player1;
+    public Player Player2;
+    public Player Player3;
+
+    // Use this for initialization
+    void Start () {
+
+        players.Add(Player0);
+        players.Add(Player1);
+        players.Add(Player2);
+        players.Add(Player3);                                      // add an item to the end of the List
+        //myList[i] = newItem;                                  // change the value in the List at position i
+        //Type thisItem = List[i];                              // retrieve the item at position i
+        //myList.RemoveAt(i);
+        print(players.Count);
+
+        foreach(Player data in players)
+        {
+            print("player: " + data);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
---
+++ republica16/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int playerID;
+    public Vector3 playerPos;
+    public float playerAngle;
+    public Vector3 playerHead;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
