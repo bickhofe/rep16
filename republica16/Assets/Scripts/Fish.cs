34658651f2a41cc140d8d8a8dd2f38677b1a4cd4
Peter Bickhofe <peter@bickhofe.de>
2016-04-20 13:56:53 +0200

    added deko und fx

---
+++ republica16/Assets/Scripts/Fish.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("fishpath"), "orientToPath", true, "lookTime", 0.5f, "time", speed, "easetype", iTween.EaseType.linear, "loopType", "loop"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
